f2661d3768511205e0becb28cab17f4673952f26
Renato Mendes <renatomcmendes@hotmail.com>
2013-06-26 19:42:05 +0100

    teste

--- js/soap.js
+++ js/soap.js
@@ -39,9 +39,80 @@ var soapTest = (function () {
             data: soapMessage,
             contentType: "text/xml; charset=UTF-8",
             dataType: 'xml',
+            cache: false,
             //async: false,
             success: function (dados, estado, jqXhr) {
 
+               var result = document.getElementById("soapResult");
+               result.innerHTML = jqXhr.responseText;
+               utils.showLoading(false);
+            },
+            error: function (jqXhr, estado, erro) {
+                alert(jqXhr.status);
+                utils.showLoading(false);
+            }
+        });
+    };
+
+    return {
+        sendMessage: sendMessage,
+        createMessage: createMessage
+    };
+
+})();
+
+var soapSap = (function () {
+
+    var soapMessage = '';
+    var soapUrl = 'http://10.0.0.52:8000/sap/bc/soap/rfc/BAPI_PO_GETITEMSREL?sap-client=300';
+    var soapAction = 'http://www.sap.com/BAPI_PO_GETITEMSREL';
+
+
+    /////////////////////////////////////////////////////////////////////
+    // Criação da mensagem xml a enviar
+    /////////////////////////////////////////////////////////////////////
+    createMessage = function () {
+
+        soapMessage = '<?xml version="1.0" encoding="utf-8"?>' +
+                        '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" ' +
+                        'xmlns:urn="urn:sap-com:document:sap:rfc:functions">' +
+                        '<soapenv:Header/>' +
+                        '<soapenv:Body>' +
+                        '<urn:BAPI_PO_GETITEMSREL>' +
+                        '<ITEMS_FOR_RELEASE>X</ITEMS_FOR_RELEASE>' +
+                        '<REL_CODE>L2</REL_CODE>' +
+                        '<REL_GROUP>A1</REL_GROUP>' +
+                        '<PO_HEADERS><item></item></PO_HEADERS>' +
+                        '<PO_ITEMS><item></item></PO_ITEMS>' +
+                        '</urn:BAPI_PO_GETITEMSREL>' +
+                        '</soapenv:Body>' +
+                        '</soapenv:Envelope>';
+
+    },
+
+
+    /////////////////////////////////////////////////////////////////////
+    // Envio da Mensagem
+    /////////////////////////////////////////////////////////////////////
+    sendMessage = function () {
+
+        utils.showLoading(true);
+
+        createMessage();
+
+        $.ajax({
+            type: 'POST',
+            url: soapUrl,
+            data: soapMessage,
+            contentType: "text/xml; charset=UTF-8",
+            dataType: "xml",
+            username: "futureview",
+            password: "fvmistica",
+            processData: false,
+            cache: false,
+            //async: false,
+            success: function (dados, estado, jqXhr) {
+              
                var result = document.getElementById("soapResult");
                result.innerHTML = jqXhr.responseText;
                utils.showLoading(false);
