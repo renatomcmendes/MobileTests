76b81dfc97908cc607d42958b9dab8082ca5d605
Renato Mendes <renatomcmendes@hotmail.com>
2013-06-27 14:59:04 +0100

    teste

--- js/soap.js
+++ js/soap.js
@@ -1,6 +1,72 @@
 var soapTest = (function () {
 
     var soapMessage = '';
+    //var soapUrl = 'futureview.no-ip.biz:8080/AddServiceServer/services/OperatorClass';
+    var soapUrl = 'http://futureview.no-ip.biz:8080/AddServiceServer/services/OperatorClass';
+
+
+    /////////////////////////////////////////////////////////////////////
+    // Criação da mensagem xml a enviar
+    /////////////////////////////////////////////////////////////////////
+    var createMessage = function () {
+
+        soapMessage = //'<?xml version="1.0" encoding="utf-8"?>' +
+                      '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:def="http://DefaultNamespace">' +
+                      '<soapenv:Header/>' +
+                      '<soapenv:Body>' +
+                      '<def:add>' +
+                      '<def:str>teste</def:str>' +
+                      '</def:add>' +
+                      '</soapenv:Body>' +
+                      '</soapenv:Envelope>';
+    
+    };
+
+
+    /////////////////////////////////////////////////////////////////////
+    // Envio da Mensagem
+    /////////////////////////////////////////////////////////////////////
+    var sendMessage = function () {
+
+
+        utils.showLoading(true);
+
+        createMessage();
+
+        $.ajax({
+            type: 'POST',
+            url: soapUrl,
+            data: soapMessage,
+            contentType: "text/xml; charset=UTF-8",
+            dataType: 'text',
+            cache: false,
+            headers: {
+                SOAPAction: ''
+            },
+            //async: false,
+            success: function (dados, estado, jqXhr) {
+
+                var result = document.getElementById("soapResult");
+                result.innerHTML = jqXhr.responseText;
+                utils.showLoading(false);
+            },
+            error: function (jqXhr, estado, erro) {
+                alert(jqXhr.status);
+                utils.showLoading(false);
+            }
+        });
+    };
+
+    return {
+        sendMessage: sendMessage,
+        createMessage: createMessage
+    };
+
+})();
+
+/*var soapTest = (function () {
+
+    var soapMessage = '';
     var soapUrl = 'http://www.webservicex.net/globalweather.asmx';
 
 
@@ -58,7 +124,8 @@ var soapTest = (function () {
         createMessage: createMessage
     };
 
-})();
+})();*/
+
 
 var soapSap = (function () {
 
