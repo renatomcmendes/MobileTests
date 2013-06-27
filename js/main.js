d56fe0242b5c73f4470a2f1573345a4bfe877454
Renato Mendes <renatomcmendes@hotmail.com>
2013-06-27 17:01:14 +0100

    sds

--- js/main.js
+++ js/main.js
@@ -1,54 +1,53 @@
-$(document).bind("pageinit", function () {
-
-
+$(document).bind("pageinit", function () { 
     $('#btnSendSoap').click(function () { soapTest.sendMessage(); });
+});
 
 
-    document.addEventListener("deviceready", function () { onDeviceReady(); }, false);
+document.addEventListener("deviceready", function () { onDeviceReady(); }, false);
 
 
-    function onDeviceReady() {
-        // Regista eventos relacionados com Phonegap
-        document.addEventListener("resume", onAppResume, false);
+function onDeviceReady() {
+    // Regista eventos relacionados com Phonegap
+    document.addEventListener("resume", onAppResume, false);
 
+};
+
+function onAppResume() {
+    alert("Resumo da Aplicação. Variaveis Limpas");
+};
+
+
+var main = (function () {
+
+    var isMobile = true;    // web app ou Phonegap app (Testes)
+
+    onBodyLoad = function (p_arg) {
+        if (isMobile) {
+            document.addEventListener("deviceready", init(), false);
+        }
+        else {
+            init();
+        }
     };
 
-    function onAppResume() {
-        alert("Resumo da Aplicação. Variaveis Limpas");
-    };
+    var init = function () {
 
+    }
 
-    var main = (function () {
-
-        var isMobile = true;    // web app ou Phonegap app (Testes)
-
-        onBodyLoad = function (p_arg) {
-            if (isMobile) {
-                document.addEventListener("deviceready", init(), false);
-            }
-            else {
-                init();
-            }
-        },
-
-    init = function () {
-
-    },
-
-    limparResultado = function () {
+    var limparResultado = function () {
         $("#soapResult").empty();
     };
 
-        // Funções Publicas
-        return {
-            onBodyLoad: onBodyLoad,
-            limparResultado: limparResultado
-        };
+    // Funções Publicas
+    return {
+        onBodyLoad: onBodyLoad,
+        limparResultado: limparResultado
+    };
 
-    })();
+})();
 
-    main.onBodyLoad();
+main.onBodyLoad();
 
-});
+
 
 
